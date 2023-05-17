9dae6f19abb0689909e400c9bee4c71dcc25efed
Turalinov <rusltur_24@mail.ru>
2023-05-17 13:16:26 +0600

    added about adaptive block

--- src/js/general.js
+++ src/js/general.js
@@ -8,3 +8,25 @@ import menu from '%modules%/menu/menu'
 
 button();
 menu();
+
+// Инициализация слайдера
+// const swiper = new Swiper('.swiper-container', {
+//   effect: 'fade',
+//   centeredSlides: true,
+//   loop: true,
+//   autoplay: {
+//     delay: 4000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//     renderBullet: function (index, className) {
+//       return '<div class="' + className + '"><span class="bullet" ></span><span>' + `${(index < 9) ? '0' : ''}`  + (index + 1) + '</span></div>';
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
