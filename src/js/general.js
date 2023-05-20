a0b157a3809159db406d65328af6335f8df0472f
Turalinov <rusltur_24@mail.ru>
2023-05-21 00:01:25 +0600

    added preloader

--- src/js/general.js
+++ src/js/general.js
@@ -1,4 +1,6 @@
 
+//preloader
+import preloader from '%modules%/preloader/preloader'
 //toggle
 import button from '%modules%/toggle/toggle'
 //menu
@@ -7,16 +9,19 @@ import menu from '%modules%/menu/menu'
 import form from '%modules%/form/form'
 //accordion
 import accordion from '%modules%/accordion/accordion'
-//swiper-g
+//gallerySlider
 import gallerySlider from '%modules%/gallery-slider/gallery-slider'
 //video
 import video from '%modules%/video/video';
 
 
-//swiper-g
+//heroSlider
 import heroSlider from '%modules%/hero-slider/hero-slider'
+//opinionSlider
+import opinionSlider from '%modules%/opinion-slider/opinion-slider'
 
 
+preloader();
 button();
 menu();
 form();
@@ -24,3 +29,4 @@ accordion();
 gallerySlider();
 video();
 heroSlider();
+opinionSlider();
